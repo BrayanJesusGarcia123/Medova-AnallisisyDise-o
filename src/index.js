4b48c3d8aab27be456d10e5438a755ca5ee248fa
MarlonPrado <marlonstivenprod@ufps.edu.co>
2023-04-02 16:53:30 -0500

    Interfaces creadas: Banco preguntas y Agregar preguntas, Rutas definidas en evaluaciones

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
-
-//Inicializacion de Express
+const {engine} = require('express-handlebars');
+const path = require('path');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/helpers')
 
-//Puerto del Servidor
+}));
+app.set('view engine', 'hbs');
+
+
+
 app.set('PORT', process.env.PORT || 4000 );
+ 
 
-
-//Inicializacion de Morgan
+//Middlewares
 app.use(morgan('dev'));
-
-
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
+ 
 //Variables Globales
+
+app.use((req,res,next) => {
+
+    next();
+});
 
 
 //Rutas
+app.use(require('./routes/routes'));
+app.use(require('./routes/register.js'));
+app.use('/evaluacion', require('./routes/evaluacion'));
+app.use(require('./routes/autentificacion'));
 
 //Public
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Arrancando el servidor
 app.listen(app.get('PORT'), () => {
     console.log('Servidor corriendo en el puerto', app.get('PORT'));
---
+++ src/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    database: {
+
+        host: 'localhost',
+        user: 'root',
+        password: '12345',
+        database: 'medova'
+
+    }
+
+
+}
